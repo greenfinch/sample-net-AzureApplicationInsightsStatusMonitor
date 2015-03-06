53b7ff5682a4b5585c2af2b5b9ca9c0e525032c2
Vas <vasily.sliouniaev@greenfinch.ie>
2015-03-06 10:34:41 +0000

    added new web project and cleaned up

---
+++ StatusMonitorSample/GlobalAssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Greenfinch Technology Ltd.")]
+[assembly: AssemblyCopyright("Greenfinch Technology Ltd. 2015")]
+[assembly: AssemblyProduct("sample-net-AzureApplicationInsightsStatusMonitor")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
---
+++ StatusMonitorSample/WebRole/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace WebRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            (new WebClient()).DownloadString("https://google.com");
+            (new WebClient()).DownloadString("https://bing.com");
+            return View();
+        }
+    }
+}
